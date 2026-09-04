56ad55b09438908c8708ce3d420b2f882244854c
Your Local Cyborg <duwareon@gmail.com>
2026-09-04 19:21:53 -0400

    Update hash for ds4 repository in package.nix

--- package.nix
+++ package.nix
@@ -54,7 +54,7 @@ stdenv.mkDerivation (finalAttrs: {
     "owner" = "antirez";
     "repo" = "ds4";
     "rev" = "b0a147a7fba6d1a104d047d5a140e9bb4bfc13cd";
-    "hash" = "sha256-aqnlAAq4Yo8f1/+vJpk4a7zLnvQG/oGWD3G7tUtjV3s=";
+    "hash" = "sha256-sHpV49J+3EPHAKFO/aKolTZ16uCfaiX+WKgsDKNLNTU=";
   };
 
 
